c3723e406156974289fde1a0cb84d73914e242c4
5f08 <5f08@liceospalla.local>
2018-05-04 10:24:03 +0200

    added .gitignore

--- index.js
+++ index.js
@@ -5,38 +5,43 @@ const api = require('./api.js')
 
 //api.accreditamento();
 
-/*api.getEs('1')
+api.getEs('1')
 .then(res => res.json())
 .then(({message, data}) => {
-	let result = 0
+	const result = []
 	data.forEach(item => {
-		result = result + item
+	 if((item % 3) === 0) result.push(item);
 	})
 	api.postEs(result, '1')
 	.then(res => res.ok ? console.log('Done') : console.log('Errore!'))
 	.catch(console.log)
 })
 .catch(console.log)
-*/
 
-/*api.getEs('2')
+
+api.getEs('2')
 .then(res => res.json())
 .then(({message, data}) => {
 	const result = []
-	let min = parseInt(data[0])
-	data.forEach(item => {if(item < min) min = item})
-	data.forEach(item => result.push(item * min))
+	data.forEach(item => {
+		if (item[item.length - 1] === "E"){
+			item = item.toLowerCase();
+			result.push(item);
+		}
+	})
 	api.postEs(result, '2')
 	.then(res => res.ok ? console.log('Done') : console.log('Errore!'))
 	.catch(console.log)
 })
-.catch(console.log)*/
+.catch(console.log)
 
 api.getEs('3')
 .then(res => res.json())
 .then(({message, data}) => {
-	const result = []
-	data.forEach(item => {if(parseInt(item) <= 3) result.push(item)})
+	let result = 0;
+	data.forEach(item => {
+		if(item.length < 5) result += item.length;
+	})
 	api.postEs(result, '3')
 	.then(res => res.ok ? console.log('Done') : console.log('Errore!'))
 	.catch(console.log)
